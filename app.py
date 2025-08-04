52c271295b2837207c02730068915cfe0dfcc576
Gacek654 <songoku8989@gmail.com>
2025-08-04 22:07:51 +0200

    Add files via upload
    Adding app.py file

--- app.py
+++ app.py
@@ -1,1 +1,67 @@
 import tkinter as tk
+from tkinter import messagebox
+import random
+
+words = ["python","java","javascript","ruby","data","science","hangman","youtube"]
+
+window = tk.Tk()
+window.title("Hangman Game")
+window.geometry("400x400")
+
+label = tk.Label(window, text = "Guess the word:", font = ("Helvetica", 16))
+label.pack()
+
+entry = tk.Entry(window)
+entry.pack()
+
+button = tk.Button(window, text="Check", command = lambda: check())
+button.pack()
+
+lives_label = tk.Label(window, text = "Lives Remaining : 6", font = ("Helvetica", 16))
+lives_label.pack()
+
+canvas = tk.Canvas(window, width = 200, height = 200)
+canvas.pack()
+
+def draw_hangman(lives):
+    if lives == 5:
+        canvas.create_line(50,50,150,50)
+    elif lives == 4:
+        canvas.create_line(100,50,100,100)
+    elif lives == 3:
+        canvas.create_oval(90,100,110,120)
+    elif lives == 2:
+        canvas.create_line(100,120,100,170)
+    elif lives == 1:
+        canvas.create_line(100,120,130,150)
+    elif lives == 0:
+        canvas.create_line(100,120,70,50)
+
+chosen_word = random.choice(words)
+guessed_word = ["_"] * len(chosen_word)
+lives = 6
+
+def check():
+    global lives
+    guess = entry.get()
+    entry.delete(0, "end")
+
+    if guess in chosen_word:
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guess:
+                guessed_word[i] = guess
+            
+        label.config(text = " ".join(guessed_word))
+
+        if "".join(guessed_word) == chosen_word:
+            messagebox.showinfo("You won!", "Congratulation, you guessed the word")
+        
+    else:
+        lives -= 1
+        lives_label.config(text=f"Lives remaining: {lives}")
+        draw_hangman(lives)
+        
+        if lives == 0:
+            messagebox.showinfo("You lost", f"The word was {chosen_word}")
+
+window.mainloop()
